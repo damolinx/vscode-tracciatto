44abee77303d3211f034d6e7f6159704c72b68de
damolinx <damolina@outlook.com>
2026-07-19 20:29:26 -0700

    Clean up API

--- src/rdbg/controllers/skipPathsSessionController.ts
+++ src/rdbg/controllers/skipPathsSessionController.ts
@@ -1,15 +1,16 @@
-import { DebugConfiguration } from '../configurations/debugConfiguration';
 import { SessionController } from './sessionController';
 
 export class SkipPathsSessionController extends SessionController {
   public override async onInitialize(): Promise<void> {
-    const config = this.session.configuration as DebugConfiguration;
-    const skipPathsLength = config.skipPaths?.length ?? 0;
-    if (skipPathsLength) {
-      await this.session.sendEvaluateRequest(
-        `DEBUGGER__::CONFIG[:skip_path] = Array(DEBUGGER__::CONFIG[:skip_path]) | ["${config.skipPaths.join('", "')}"]`,
-      );
+    const { skipPaths } = this.session.configuration;
+    const skipPathsLength = skipPaths?.length ?? 0;
+    if (!skipPathsLength) {
+      return;
     }
+
+    await this.session.sendEvaluateRequest(
+      `DEBUGGER__::CONFIG[:skip_path] = Array(DEBUGGER__::CONFIG[:skip_path]) | ["${skipPaths.join('", "')}"]`,
+    );
     this.context.log.debug(`[${this.session.shortId}] Initialized skip-paths (${skipPathsLength})`);
   }
 }
